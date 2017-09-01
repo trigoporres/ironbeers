d2c9b122cc39427ed0f2cbf06456a046f91e77cb
trigoporres <santiagotrigoporres@gmail.com>
2017-09-01 13:21:08 +0200

    perdido

--- js/board.js
+++ js/board.js
@@ -1,10 +1,1 @@
-function Board(){
-  this.position = [
-    [T,T,T,T,T,T,F],
-    [F,F,F,F,F,T,T],
-    [T,T,T,T,T,T,F],
-    [F,F,F,F,F,T,T],
-    [T,T,T,T,T,T,F],
-    [F,F,F,F,F,T,T],
-  ];
-};
+
